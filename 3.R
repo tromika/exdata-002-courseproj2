870fd0c07bffa9e8b46770bbe2873f3ccb704c9e
Tamas Szuromi <tromika@gmail.com>
2014-05-25 20:13:54 +0200

    4-6 R script done

--- 3.R
+++ 3.R
@@ -6,10 +6,11 @@ library(reshape2)
 NEI.baltimore <- NEI[NEI$fips==24510,]
 
 agg <- aggregate(NEI.baltimore$Emissions,by=list(NEI.baltimore$year,NEI.baltimore$type),FUN=sum)
-
-p <- qplot(Group.1,x,
+png(file = "plots/plot3.png")
+qplot(Group.1,x,
            facets=.~Group.2,
            data=agg,
            geom=c('point','smooth'), method='lm',
            main='PM25 Emissions - Baltimore',
            xlab='year', color=Group.2)
+dev.off()
